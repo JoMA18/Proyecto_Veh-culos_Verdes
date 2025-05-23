4af56908e034cdf024a103bb4241435f34ea561e
JoMA18 <A01709351@tec.mx>
2025-05-23 00:00:36 -0600

    Create vehiculos.h

---
+++ vehiculos.h
@@ -0,0 +1,54 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+/*Clase madre*/ 
+class Vehiculo {
+protected:
+    string matricula;
+    string tipo;
+
+public:
+    Vehiculo(string matricula, string tipo) : matricula(matricula), tipo(tipo) {}
+    virtual string Info() const = 0; // Método abstracto
+};
+
+/* Clases hijas 
+Clase Bicicleta*/
+class Bicicleta : public Vehiculo {
+private:
+    string color;
+    string tamaño;
+
+public:
+    Bicicleta(string matricula, string tipo, string color, string tamaño) : Vehiculo(matricula, tipo), color(color), tamaño(tamaño) {}
+    string Info() const override {
+        return "Vehículo: Bicicleta - Matricula: " + matricula + ", Tipo: " + tipo + ", Color: " + color + ", Tamaño: " + tamaño;
+    }
+};
+
+/* Clase Patin */
+class Patin : public Vehiculo {
+private:
+    string marca;
+
+public:
+    Patin(string matricula, string tipo, string marca) : Vehiculo(matricula, tipo), marca(marca) {}
+    string Info() const override {
+        return "Vehículo: Patín - Matricula: " + matricula + ", Tipo: " + tipo + ", Marca: " + marca;
+    }
+};
+
+/* Clase Patineta*/
+class Patineta : public Vehiculo {
+private:
+    string tamaño;
+
+public:
+    Patineta(string matricula, string tipo, string tamaño) : Vehiculo(matricula, tipo), tamaño(tamaño) {}
+    string Info() const override {
+        return "Vehículo: Patineta - Matricula: " + matricula + ", Tipo: " + tipo + ", Tamaño: " + tamaño;
+    }
+};
+
