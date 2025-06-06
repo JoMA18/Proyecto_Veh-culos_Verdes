dcd6230e313527c18c1260c17a7efe6b8c55fee6
JoMA18 <A01709351@tec.mx>
2025-06-06 10:42:14 -0600

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,27 +1,26 @@
 #include <iostream>   
 
 #include "vehiculos.h" 
-
 #include "empresa.h"
 
 using namespace std;
 
 int main() {
 
-
-  Empresa empresa;
-  empresa.creaVehiculo();
-  empresa.muestraVehiculo();
-
-  cout << "test \n\n";
-  empresa.muestraVehiculo("bicicleta");
-
-  empresa.agregaBici("07", "bicicleta", "Rojo", "Chica");
-  empresa.muestraVehiculo("bicicleta");
-  empresa.agregaPatin("09", "patin", "Fila");
-  empresa.muestraVehiculo("patin");
-  empresa.agregaPatineta("12", "patineta", "Chica");
-  empresa.muestraVehiculo("patineta");
   
-  cout << "test \n\n";
+  Empresa em1;
+  em1.creaVehiculo();
+  em1.muestraVehiculo();
+
+
+  em1.muestraVehiculo("bicicleta");
+
+  em1.agregaBici("07", "bicicleta", "Rojo", "Chica");
+  em1.muestraVehiculo("bicicleta");
+  em1.agregaPatin("09", "patin", "Fila");
+  em1.muestraVehiculo("patin");
+  em1.agregaPatineta("12", "patineta", "Chica");
+  em1.muestraVehiculo("patineta");
+  
+}
 }
