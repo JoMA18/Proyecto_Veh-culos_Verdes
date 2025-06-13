9ba9833e12fcd23d8f61d347bc13efe43a0025c1
JoMA18 <A01709351@tec.mx>
2025-06-13 00:00:04 -0600

    Update vehiculos.h

--- vehiculos.h
+++ vehiculos.h
@@ -1,3 +1,8 @@
+// Proyecto de Vehiculos Verdes.
+// Clase Vehiculos donde se encapsula la herencia, polimorfismo
+// y clases abstractas
+
+
 #ifndef VEHICULOS_H_
 #define VEHICULOS_H_
 
@@ -7,25 +12,29 @@
 
 using namespace std;
 
-// Clase Madre
+// Clase Padre - VEHICULO
+// Esta clase es abstracta y define los vehículos
 class Vehiculo {
 protected:
     string matricula;
     string tipo;
 
 public:
-    Vehiculo() : matricula(""), tipo("") {}
-    Vehiculo(string m, string t) : matricula(m), tipo(t) {}
+    // Se implementa el método de sobrecarga
+    Vehiculo() : matricula(""), tipo("") {} // Constructor por defecto
+    Vehiculo(string m, string t) : matricula(m), tipo(t) {} // Constructor 
+    //con parámetros
 
-    virtual string Info() {
-        return "Vehiculo - Matricula: " + matricula + ", Tipo: " + tipo + "\n";
-    }
+    virtual string Info() = 0; // Método virtual puro, hace de Vehiculo 
+    //una clase abstracta
+    string get_matricula() {return matricula;}
     string get_tipo() {return tipo;}
 };
 
 
 
 // Clase Hija - BICI
+// Esta clase hereda de Vehiculo 
 class Bicicleta : public Vehiculo {
 private:
     string color;
@@ -36,13 +45,18 @@ public:
     Bicicleta(string m, string t, string c, string tz)
         : Vehiculo(m, t), color(c), tamaño(tz) {}
 
-    string Info() {
-        return "Bicicleta - Matricula: " + matricula + ", Tipo: " + tipo +
-               ", Color: " + color + ", Tamaño: " + tamaño + "\n";
-    }
-};
+    string Info(); // Método que devuelve información de la bicicleta, brinda 
+};  //Se usa la sobreescritura para cambiar el comportamiento del método Info 
+    //de la clase padre
+
+/*Metodo Bicicleta*/
+string Bicicleta::Info() {
+    return "Bicicleta - Matricula: " + matricula + ", Tipo: " + tipo +
+           ", Color: " + color + ", Tamaño: " + tamaño + "\n";
+}
 
 // Clase Hija - PATIN
+// Esta clase hereda de Vehiculo 
 class Patin : public Vehiculo {
 private:
     string marca;
@@ -52,13 +66,16 @@ public:
     Patin(string m, string t, string mar)
         : Vehiculo(m, t), marca(mar) {}
 
-    string Info() {
-        return "Patin - Matricula: " + matricula + ", Tipo: " + 
-        tipo + ", Marca: " + marca + "\n";
-    }
+    string Info();
 };
 
+string Patin::Info() {
+    return "Patin - Matricula: " + matricula + ", Tipo: " + tipo +
+           ", Marca: " + marca + "\n";
+}
+
 // Clase patineta - Patineta
+// Esta clase hereda de Vehiculo
 class Patineta : public Vehiculo {
 private:
     string tamaño;
@@ -68,10 +85,12 @@ public:
     Patineta(string m, string t, string tz)
         : Vehiculo(m, t), tamaño(tz) {}
 
-    string Info() {
-        return "Patineta - Matricula: " + matricula + ", Tipo: " + 
-        tipo + ", Tamaño: " + tamaño + "\n";
-    }
+    string Info();
 };
 
-#endif
+string Patineta::Info() {
+    return "Patineta - Matricula: " + matricula + ", Tipo: " + tipo +
+           ", Tamaño: " + tamaño + "\n";
+}
+
+#endif // VEHICULOS_H_
